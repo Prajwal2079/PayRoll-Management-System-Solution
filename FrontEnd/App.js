cfe14d1907612ea8f4abc0f7e31b1824322984d9
Prajwal Patil <114739626+Prajwal2079@users.noreply.github.com>
2022-12-21 23:17:19 +0530

    Create App.js

---
+++ FrontEnd/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from './component/Login'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Login></Login>
+    </div>
+  );
+}
+
+export default App;
