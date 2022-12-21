619787b7e2c1e59741ff092dd2bff08f62320540
Prajwal Patil <114739626+Prajwal2079@users.noreply.github.com>
2022-12-21 23:18:12 +0530

    Create Login.js

---
+++ FrontEnd/LoginPage/Login.js
@@ -0,0 +1,33 @@
+import { PersonCircle , KeyFill} from 'react-bootstrap-icons'
+import './Login.css'
+
+const Login = () => {
+
+    return (
+        <div className="loginbody">
+        <h1 className="header">Payroll Management System</h1>
+        <br/><br/>
+            <div className="outerdiv">
+            <form action="" onSubmit="return authenticate()">
+                <div className="form-floating mb-3">
+                    <label htmlFor="empid"><h3><PersonCircle></PersonCircle>&nbsp;&nbsp;Employee ID</h3></label>
+                    <input type="text" className="form-control" id="empid" placeholder="ABC001" required />
+
+                </div>
+                <div class="form-floating">
+                    <label htmlFor="password"><h3><KeyFill></KeyFill>&nbsp;&nbsp;Password</h3></label>
+                    <input type="password" className="form-control" id="password" placeholder="Password" required />
+
+                </div>
+                <div>
+                    <br/>
+                    <button type="submit" className="btn btn-primary">Login</button>
+                </div>
+            </form>
+            </div>
+        </div>
+    )
+
+
+}
+export default Login
