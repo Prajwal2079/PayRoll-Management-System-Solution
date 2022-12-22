2f76ec34f0f10f2f3d6bf4085783836d6bf2a13c
PrashantChede <chedeprashant2022@gmail.com>
2022-12-22 15:43:47 +0530

    changes done by prashant

---
+++ IACSD_229183/server1.js
@@ -0,0 +1,10 @@
+var express=require("express");
+var app=express();
+app.get("/",(req,resp)=>{
+
+    resp.send("<h2>Welcome to IACSD !!");
+    resp.end();
+
+});
+app.listen(5000);
+console.log("server is listening on 3000 !!");
