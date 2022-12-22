278176e219126efc3c96df230ef3112cfda7db5c
UTKARSH UPADHYAY <113631380+UtkarshUpadhyay1703@users.noreply.github.com>
2022-12-22 16:54:58 +0530

    Deleted to improve

--- 229107/Server.js
+++
@@ -1,8 +0,0 @@
-    const express=require("express");
-    const app=express();
-    app.get("/",function(req,resp){
-        console.log("inside Server");
-        resp.send("<h1>This is under Server</h1>");
-    })
-    app.listen(3000);
-    console.log("This code is running on port number 3000");
